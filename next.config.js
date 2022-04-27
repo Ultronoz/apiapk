e340a9b3b022ecc1e225bd92114cb6bc68ae61e8
Ultronoz <diegonadalettiult@gmail.com>
2022-04-27 13:52:37 -0300

    5

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/app/cep",
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "file:///C:/Users/diego/Desktop/teste.html" },
-        ],
-      },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
-}
+  }
+};
